3c1a4c2fa51ce3eb53f3525ed14a69883249848d
Razikov Farrukh <raz_far@mail.ru>
2020-10-31 19:39:50 +0300

    Secret key moved out from the project

--- karma/settings.py
+++ karma/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from secrets import secret_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$1(---=$m)24caiz_7ip4x6mx)*h45+9g#1=1^zba)k^(m!6lv'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
