7cfc9dfb2c3b5683fd4a94e081a79e15f250bbb9
Razikov Farrukh <raz_far@mail.ru>
2020-11-01 02:05:55 +0300

    Созданно базовое наследование шаблонов. Временно убраны не работающие вкладки

--- static/mainapp/js/main.js
+++ static/mainapp/js/main.js
@@ -54,7 +54,7 @@ $(document).ready(function(){
         autoplayTimeout: 5000,
         loop:true,
         nav:true,
-        navText:["<img src='img/banner/prev.png'>","<img src='img/banner/next.png'>"],
+        navText:["<img src='/static/mainapp/img/banner/prev.png/'>", "<img src='/static/mainapp/img/banner/next.png'>"],
         dots:false
     });
 
@@ -67,7 +67,7 @@ $(document).ready(function(){
         autoplayTimeout: 5000,
         loop:true,
         nav:true,
-        navText:["<img src='img/product/prev.png'>","<img src='img/product/next.png'>"],
+        navText:["<img src='/static/mainapp/img/product/prev.png'>","<img src='/static/mainapp/img/product/next.png'>"],
         dots:false
     });
 
@@ -92,7 +92,7 @@ $(document).ready(function(){
         autoplayTimeout: 5000,
         loop:true,
         nav:true,
-        navText:["<img src='img/product/prev.png'>","<img src='img/product/next.png'>"],
+        navText:["<img src='/static/mainapp/img/product/prev.png'>","<img src='/static/mainapp/img/product/next.png'>"],
         dots:false
     });
 
