5f7f6ba81ce0d005592de1a36ef6c93eca6a53aa
Razikov Farrukh <raz_far@mail.ru>
2020-11-02 02:30:21 +0300

    Блоки js и css вынесены в отдельный шаблон. Добавлена проверка 'активности' страницы через request.resolver_match. Дополнены json

--- mainapp/views.py
+++ mainapp/views.py
@@ -7,19 +7,38 @@ from karma.settings import BASE_DIR
 DATA_DIR = os.path.join(BASE_DIR, 'data', 'mainapp')
 
 
+def get_related_products() -> dict:
+    with open(os.path.join(DATA_DIR, 'related_products.json')) as f:
+        related_products_data = json.load(f)
+    related_products = related_products_data.get("related_products") or dict()
+    return related_products
+
+
 def index(request):
     with open(os.path.join(DATA_DIR, 'index.json')) as f:
-        context = json.load(f)
+        index_data = json.load(f)
+    context = dict(
+        related_products=get_related_products,
+        page_title=index_data.get("page_title") or "Karma"
+    )
     return render(request, 'mainapp/index.html', context=context)
 
 
 def category(request):
     with open(os.path.join(DATA_DIR, 'category.json')) as f:
-        context = json.load(f)
+        category_data = json.load(f)
+    context = dict(
+        related_products=get_related_products,
+        page_title=category_data.get("page_title") or "Karma"
+    )
     return render(request, 'mainapp/category.html', context=context)
 
 
 def single_product(request):
     with open(os.path.join(DATA_DIR, 'single-product.json')) as f:
-        context = json.load(f)
+        single_product_data = json.load(f)
+    context = dict(
+        related_products=get_related_products,
+        page_title=single_product_data.get("page_title") or "Karma"
+    )
     return render(request, 'mainapp/single-product.html', context=context)
