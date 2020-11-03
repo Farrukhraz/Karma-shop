76fa11ae54997bb89ec6fb51c2243c0bef39e33f
Razikov Farrukh <raz_far@mail.ru>
2020-11-04 01:03:39 +0300

    secret key вынесен в переменные окружения

--- karma/settings.py
+++ karma/settings.py
@@ -23,7 +23,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret_key
+SECRET_KEY = os.environ.get('DJANGO_KARMA_SK')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
