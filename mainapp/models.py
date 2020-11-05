3e56874620dde315389f3dce961062d4268f7ffb
Razikov Farrukh <raz_far@mail.ru>
2020-11-05 20:54:13 +0300

    Django models

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(verbose_name="name", max_length=64, unique=True)
+    description = models.TextField(verbose_name="description", blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductBrand(models.Model):
+    name = models.CharField(verbose_name="name", max_length=64, unique=True)
+    description = models.TextField(verbose_name="description", blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='product name', max_length=64)
+    brand_name = models.ForeignKey(ProductBrand, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products_images', blank=True)
+    short_desc = models.CharField(verbose_name='product short description', max_length=60, blank=True)
+    description = models.TextField(verbose_name='product description', blank=True)
+    price = models.DecimalField(verbose_name='product price', max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name='quantity in stock', default=0)
+
+    def __str__(self):
+        return f"{self.name} - Category:({self.category.name}); Brand: ({self.brand_name})"
