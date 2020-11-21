4ed3be53006bc47e3a75d710a0f8d44519f7802d
Farrukh Razikov <raz_far@mail.ru>
2020-11-21 14:04:35 +0300

    lil changes in authapp db table

--- authapp/models.py
+++ authapp/models.py
@@ -4,8 +4,6 @@ from django.db import models
 class AuthPageImages(models.Model):
 
     name = models.CharField(verbose_name='image name', max_length=64, blank=True)
-    title = models.CharField(verbose_name="image title", max_length=64, blank=True)
-    description = models.TextField(verbose_name="image description", blank=True)
     image = models.ImageField(upload_to="auth_page_images", blank=True)
 
     def __str__(self):
