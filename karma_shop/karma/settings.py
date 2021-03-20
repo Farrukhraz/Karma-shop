c94d01ef171705853f25f98de5c3db092adec5ec
Razikov Farrukh <raz_far@mail.ru>
2021-03-20 18:58:27 +0300

    Дополнен readme. Добавлена возможность запуска приложения в docker контейнере

--- karma_shop/karma/settings.py
+++ karma_shop/karma/settings.py
@@ -86,7 +86,6 @@ WSGI_APPLICATION = 'karma.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
